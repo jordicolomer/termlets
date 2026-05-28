3cbadebbc8ddfb488053aebad329b69e338f10a1
Jordi Colomer <jordikolomer@gmail.com>
2026-05-28 19:02:50 +0100

    fix compile

--- slider.h
+++ slider.h
@@ -10,6 +10,16 @@ typedef struct Slider_data
   int virtual_height;
 } Slider_data;
 
+/*
+typedef struct Slider_data
+{
+  Window *slider_grip;
+  Window *child;
+  int height;
+  int virtual_height;
+} Slider_data;
+
+*/
 
 void Slider_hover(Window *wg, int x, int y);
 void Slider_grip_hover(Window *wg, int x, int y);
