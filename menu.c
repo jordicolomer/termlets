297fece0e44ecb9fadecca9b608f2742406c7280
Jordi Colomer <jordikolomer@gmail.com>
2026-07-02 17:59:19 +0100

    window list with Menu_add_element

--- menu.c
+++ menu.c
@@ -172,12 +172,14 @@ void Menu_list_windows_item_selected(Tab *tab, Window *menu)
     Window_remove(menu);
 }
 
-Window *Menu_list_windows(Window *self, Tabs *tabs, struct Window *menuItem)
+Window *Menu_list_windows(Window *menu, Tabs *tabs, struct Window *window_menu_item)
 {
-    Window *menu = malloc(sizeof *menu);
+    /*Window *menu = malloc(sizeof *menu);
     Window_init(menu, -1, -1, -1, -1, -1, -1);
     menu->left = menuItem->calculated.x - self->calculated.x;
-    menu->top = menuItem->calculated.y - self->calculated.y + 1;
+    menu->top = menuItem->calculated.y - self->calculated.y + 1;*/
+    Window *submenu = Menu_create_vertical(NULL, menu);
+
     Tab *tab = tabs->first;
     int i = 0;
     //Window_add_widget(menu, 0, 0, i++, -1, -1, 1, "", 232, 15);
@@ -190,39 +192,44 @@ Window *Menu_list_windows(Window *self, Tabs *tabs, struct Window *menuItem)
             selected_sign = "*";
         asprintf(&tab_label, " %s%s  ", selected_sign, tab->child->id);
 
-        Window *win = Window_add_widget(menu, 0, 0, i++, -1, -1, 1, tab_label, 232, 253);
+        //Window *win = Window_add_widget(menu, 0, 0, i++, -1, -1, 1, tab_label, 232, 253);
 
-        win->on_hover = Menu_change_color_hover;
-        win->undo_on_hover = Menu_change_color_normal;
         //win->data = tab;
         //win->data2 = menu;
         //win->on_mouse_down = Menu_menu_windows_item;
-        win->lambda = create_lambda(Menu_list_windows_item_selected, 2, tab, menu);
+        Window *win = Menu_add_element(submenu, tab_label, NULL);
+        win->on_hover = Menu_change_color_hover;
+        win->undo_on_hover = Menu_change_color_normal;
+        win->lambda = create_lambda(Menu_list_windows_item_selected, 2, tab, submenu);
         win->on_mouse_down = Execute_lambda;
 
         maxLen = max(maxLen, strlen(tab_label));
         tab = tab->next;
     }
-    Window_add_widget(menu, 0, 0, i++, -1, -1, 1, "", 232, 253);
-    menu->width = maxLen;
-    menu->height = i;
+    Menu_add_element(submenu, "", NULL);
+    submenu->left = window_menu_item->left;
+    submenu->top = window_menu_item->top + 1;
+    submenu->hidden = 0;
+    //Window_add_widget(menu, 0, 0, i++, -1, -1, 1, "", 232, 253);
+    //menu->width = maxLen;
+    //menu->height = i;
 
-    Window_append(self, menu);
+    //Window_append(self, menu);
     //Menu * menu = Menu_create_vertical(self);
 }
 
-void Menu_add_windows2(Menu *self, char *name, Tabs *tabs)
+void Menu_add_windows2(Menu *menu, char *name, Tabs *tabs)
 {
     // Window * Menu_add_element(Menu * self, char * name, Menu_lambda);
     int len = strlen(name);
-    self->offset += 1;
-    Window *window_menu_item = Window_add_widget(self, self->offset, -1, 0, -1, len, 1, name, 232, 253);
-    self->offset += len + 1;
+    menu->offset += 1;
+    Window *window_menu_item = Window_add_widget(menu, menu->offset, -1, 0, -1, len, 1, name, 232, 253);
+    menu->offset += len + 1;
 
     //window->data = Menu_menu_windows;
     //window->data2 = self->parent;
     //window->data3 = tabs;
-    window_menu_item->lambda = create_lambda(Menu_list_windows, 3, self, tabs, window_menu_item);
+    window_menu_item->lambda = create_lambda(Menu_list_windows, 3, menu, tabs, window_menu_item);
     window_menu_item->on_mouse_down = Execute_lambda;
 }
 
