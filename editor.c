b9fbac6f4c3eefc0678f2c5b422d4b095ef0f894
Jordi Colomer <jordikolomer@gmail.com>
2026-08-20 22:21:54 +0100

    set x on EditorWindow_on_mouse_down

--- editor.c
+++ editor.c
@@ -1050,6 +1050,8 @@ void EditorWindow_draw(struct Window *w, int hasFocus)
 void EditorWindow_on_mouse_down(Window *win, int x, int y){
   EditorWindow *self = win;
   self->cursor.n = y - win->shift - win->calculated.y;
+  self->cursor.x = x - win->calculated.x;
+  EditorWindow_fix_cursor_x(self);	
   EditorWindow_make_cursor_visible(self);
 }
 
