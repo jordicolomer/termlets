d86c93aefdcbdbefb851f66d22810e2625be3c8a
Jordi Colomer <jordikolomer@gmail.com>
2026-09-12 13:08:13 +0100

    reload shortcut

--- editor.c
+++ editor.c
@@ -1281,7 +1281,7 @@ Window *Editor_menu(EditorFrame *self)
     Window *menu = Menu_create_horizontal();
 
     Window *file = Menu_create_vertical(self);
-    Menu_add_element(file, " 📄 New    Ctrl+N", create_lambda(Editor_menu_new, 1, self));
+    //Menu_add_element(file, " 📄 New    Ctrl+N", create_lambda(Editor_menu_new, 1, self));
     Menu_add_element(file, " 🔄 Reload Ctrl+R", create_lambda(Editor_on_selected, 2, self, EditorWindow_reload));
     Menu_add_element(file, " 💾 Save   Ctrl+S", create_lambda(Editor_on_selected, 2, self, EditorWindow_save));
     Menu_add_element(file, " ❌ Close  Ctrl+W", create_lambda(Editor_menu_new, 1, self));
